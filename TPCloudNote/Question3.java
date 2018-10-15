eb25a1ebc4f9bf93b3ef558fb154df45b36cfe5d
demetre.phalavnadihshvili@gmail.com <demeph@Ideapad.localdomain>
2018-10-15 15:36:40 +0200

    add Q3

--- TPCloudNote/Question3.java
+++ TPCloudNote/Question3.java
@@ -8,11 +8,13 @@ import com.vmware.vim25.HostHardwareInfo;
 import com.vmware.vim25.InvalidProperty;
 import com.vmware.vim25.RuntimeFault;
 import com.vmware.vim25.mo.HostSystem;
+import com.vmware.vim25.mo.VirtualMachine;
+import com.vmware.vim25.VirtualMachineConfigInfo;
 import com.vmware.vim25.mo.InventoryNavigator;
 import com.vmware.vim25.mo.ManagedEntity;
 import com.vmware.vim25.mo.ServiceInstance;
 
-public class ListHosts {
+public class Question3 {
 	static final String SERVER_NAME = "10.1.0.4";
 	static final String USER_NAME = "root";
 	static final String PASSWORD = "adminroot@";
@@ -22,19 +24,15 @@ public class ListHosts {
 		// List host systems
 		try {
 			ServiceInstance si = new ServiceInstance(new URL(url), USER_NAME, PASSWORD, true);
-			ManagedEntity[] managedEntities = new InventoryNavigator(si.getRootFolder()).searchManagedEntities("HostSystem");
+			ManagedEntity[] managedEntities = new InventoryNavigator(si.getRootFolder()).searchManagedEntities("VirtualMachine");
 			for (ManagedEntity managedEntity : managedEntities) {
-				HostSystem host = (HostSystem) managedEntity;
-				System.out.println("Host: '" + host.getName() + "'");
-				HostHardwareInfo hw = host.getHardware();
-				System.out.println("Model: " + hw.getSystemInfo().getModel());
-				System.out.println("Memory in Bytes: " + hw.getMemorySize());
-				System.out.println("# of CPU Cores: " + hw.getCpuInfo().getNumCpuCores());
-				HostCpuPackage[] cpuPkg = hw.getCpuPkg();
-				for (int i = 0; i < cpuPkg.length; i++) {
-					HostCpuPackage pkg = cpuPkg[i];
-					System.out.println(pkg.getIndex() + " : " + pkg.getDescription() + " (vendor: " + pkg.getVendor() + ")");
-				}
+				VirtualMachine vm = (VirtualMachine) managedEntity;
+				System.out.println("VM: '" + vm.getName() + "'");
+				VirtualMachineConfigInfo vmInfo = (VirtualMachineConfigInfo) vm.getConfig();
+				System.out.println("-- Config :");
+//				System.out.println("---Fullname : " +vmInfo.guestFullName());
+				System.out.println("---Version : "+ vmInfo.version);
+				System.out.println("---CPU :  "+vmInfo.getHardware().numCPU+" vCPU \n---Memory : " +vmInfo.getHardware().memoryMB +" MB"); 
 			}
 			si.getServerConnection().logout();
 		} catch (InvalidProperty e) {
