f91ec07b256736ea49a3cd9572aa6ab7eb1907c5
demetre.phalavnadihshvili@gmail.com <demeph@Ideapad.localdomain>
2018-10-15 15:59:15 +0200

    Commit

--- TPCloudNote/Question4.java
+++ TPCloudNote/Question4.java
@@ -15,7 +15,7 @@ import com.vmware.vim25.mo.InventoryNavigator;
 import com.vmware.vim25.mo.ManagedEntity;
 import com.vmware.vim25.mo.ServiceInstance;
 
-public class Question3 {
+public class Question4 {
 	static final String SERVER_NAME = "10.1.0.4";
 	static final String USER_NAME = "root";
 	static final String PASSWORD = "adminroot@";
@@ -29,34 +29,7 @@ public class Question3 {
 			for (ManagedEntity managedEntity : managedEntities) {
 				VirtualMachine vm = (VirtualMachine) managedEntity;
 				System.out.println("VM: '" + vm.getName() + "'");
-				VirtualMachineConfigInfo vmInfo = (VirtualMachineConfigInfo) vm.getConfig();
-				for (VirtualDevice vd : vmInfo.getHardware().getDevice()) { 
-  
-			        if (vd instanceof VirtualMachineVideoCard) { 
-			            VirtualMachineVideoCard vVideoCard = (VirtualMachineVideoCard) vd; 
-			        } else if (vd instanceof VirtualMachineVMCIDevice) { 
-			            VirtualMachineVMCIDevice vVMCI = (VirtualMachineVMCIDevice) vd; 
-			        } else if (vd instanceof VirtualSCSIPassthrough) { 
-			            VirtualSCSIPassthrough vSCSI = (VirtualSCSIPassthrough) vd; 
-			        } else if (vd instanceof VirtualDisk) { 
-			            VirtualDisk vDisk = (VirtualDisk) vd; 
-			            VirtualDiskFlatVer2BackingInfo vbi = (VirtualDiskFlatVer2BackingInfo) vd.getBacking(); 
-			            System.out.println("--VirtualDisk : \n---Capacity : "+vDisk.capacityInKB + " KB");
-			        } else if (vd instanceof VirtualCdrom) { 
-			            VirtualCdrom vCDrom = (VirtualCdrom) vd; 
-			            if (vCDrom.getBacking() instanceof VirtualCdromRemoteAtapiBackingInfo) { 
-			            } else if (vCDrom.getBacking() instanceof VirtualCdromAtapiBackingInfo) { 
-			                 VirtualCdromAtapiBackingInfo vabi = (VirtualCdromAtapiBackingInfo) vCDrom.getBacking(); 
-			            } else if (vCDrom.getBacking() instanceof VirtualCdromIsoBackingInfo) { 
-			                 VirtualCdromIsoBackingInfo vibi = (VirtualCdromIsoBackingInfo) vCDrom.getBacking(); 
-			            } else if (vCDrom.getBacking() instanceof VirtualCdromRemotePassthroughBackingInfo) { 
-			                 VirtualCdromRemotePassthroughBackingInfo vpbi = (VirtualCdromRemotePassthroughBackingInfo) vCDrom.getBacking(); 
-			            } 
-			        } else if (vd instanceof VirtualEthernetCard) { 
-			            VirtualEthernetCard vEth = (VirtualEthernetCard) vd; 
-	 				     if (vEth instanceof VirtualE1000) {  
-	 				         System.out.println("---Adapter type: E1000");  
-	 				     } else if (vEth instanceof VirtualPCNet32) {  
+xanceof VirtualPCNet32) {  
 	 				         System.out.println("---Adapter type: PCnet32");  
 	 				     } else if (vEth instanceof VirtualVmxnet) {  
 	 				         System.out.println("---Adapter type: Vmxnet");  
